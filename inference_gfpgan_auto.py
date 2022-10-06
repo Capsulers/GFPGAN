e7b3b8b36ad1598ec971ebd61bed3bc7fdc2929b
Capsulers <113909207+Capsulers@users.noreply.github.com>
2022-10-06 17:26:24 +0800

    Update inference_gfpgan_auto.py

--- inference_gfpgan_auto.py
+++ inference_gfpgan_auto.py
@@ -52,7 +52,7 @@ def main():
     parser.add_argument(
         '--ext',
         type=str,
-        default='png',
+        default='auto',
         help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs. Default: auto')
     parser.add_argument('-w', '--weight', type=float, default=0.5, help='Adjustable weights.')
     args = parser.parse_args()
@@ -99,3 +99,103 @@ def main():
                 model_path='https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth',
                 dni_weight=dni_weight,
                 model=model,
+                tile=args.bg_tile,
+                tile_pad=10,
+                pre_pad=0,
+                half=True)
+    else:
+        bg_upsampler = None
+    # ------------------------ set up GFPGAN restorer ------------------------
+    if args.version == '1':
+        arch = 'original'
+        channel_multiplier = 1
+        model_name = 'GFPGANv1'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+    elif args.version == '1.2':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANCleanv1-NoCE-C2'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
+    elif args.version == '1.3':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.3'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+    elif args.version == '1.4':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.4'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+    elif args.version == 'RestoreFormer':
+        arch = 'RestoreFormer'
+        channel_multiplier = 2
+        model_name = 'RestoreFormer'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+    else:
+        raise ValueError(f'Wrong model version {args.version}.')
+
+    # determine model paths
+    model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        model_path = os.path.join('gfpgan/weights', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        # download pre-trained models from url
+        model_path = url
+
+    restorer = GFPGANer(
+        model_path=model_path,
+        upscale=args.upscale,
+        arch=arch,
+        channel_multiplier=channel_multiplier,
+        bg_upsampler=bg_upsampler)
+
+    # ------------------------ restore ------------------------
+    for img_path in img_list:
+        # read image
+        img_name = os.path.basename(img_path)
+        print(f'Processing {img_name} ...')
+        basename, ext = os.path.splitext(img_name)
+        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        # restore faces and background if necessary
+        cropped_faces, restored_faces, restored_img = restorer.enhance(
+            input_img,
+            has_aligned=args.aligned,
+            only_center_face=args.only_center_face,
+            paste_back=True,
+            weight=args.weight)
+
+        # save faces
+        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+            # save cropped face
+            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
+            imwrite(cropped_face, save_crop_path)
+            # save restored face
+            if args.suffix is not None:
+                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
+            else:
+                save_face_name = f'{basename}_{idx:02d}.png'
+            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
+            imwrite(restored_face, save_restore_path)
+            # save comparison image
+            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+
+        # save restored img
+        if restored_img is not None:
+            if args.ext == 'auto':
+                extension = ext[1:]
+            else:
+                extension = args.ext
+
+            if args.suffix is not None:
+                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
+            else:
+                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}.{extension}')
+            imwrite(restored_img, save_restore_path)
+
+    print(f'Results are in the [{args.output}] folder.')
+
+
+if __name__ == '__main__':
+    main()
